8446a196ac5394f35272e86657024308928ad98f
Robbert Harms <robbert@xkls.nl>
2016-01-10 12:23:59 +0100

    Adds some more image output post-processing

--- musical_games/converters/images.py
+++ musical_games/converters/images.py
@@ -42,7 +42,7 @@ def draw_rectangle(image_fname, fill_color, stroke_color, stroke_width, position
 
     output_fname = output_fname or image_fname
 
-    command = ['convert', '-quality', '95', '-fill', fill_color, '-stroke', stroke_color,
+    command = ['convert', '-quality', '100', '-fill', fill_color, '-stroke', stroke_color,
                '-strokewidth', str(stroke_width),
                '-draw', 'rectangle ' + ','.join(map(str, position)) + ' ' + ','.join(map(str, end_position)),
                image_fname, output_fname]
